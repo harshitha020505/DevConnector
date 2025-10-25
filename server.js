45f02ab8c9eb0b4df25c8d724b467fddec234bbb
harshitha020505 <harshithabobbala392@gmail.com>
2025-10-25 21:55:37 +0530

    addedd routes

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const connectDB=require('./config/db');
 const app=express();
 connectDB();
 app.get('/',(req,res)=>res.send('API Running'));
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/profile',require('./routes/api/profile'));
+app.use('/api/auth',require('./routes/api/auth'));
 const PORT=process.env.port||5000;
 app.listen(PORT,()=>{
     console.log(`server started at ${PORT}`);
