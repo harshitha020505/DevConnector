b5a3fda87f65d0efc3460b968a0cac19d2b72759
harshitha020505 <harshithabobbala392@gmail.com>
2025-10-25 21:58:59 +0530

    added routes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.get('/',(req,res)=>res.send('API Running'));
 app.use('/api/users',require('./routes/api/users'));
 app.use('/api/profile',require('./routes/api/profile'));
 app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/posts',require('./routes/api/posts'));
 const PORT=process.env.port||5000;
 app.listen(PORT,()=>{
     console.log(`server started at ${PORT}`);
