ff67a3c9f0520541ecae2dc252b1506fd3ba8cc1
harshitha020505 <harshithabobbala392@gmail.com>
2025-10-25 17:26:04 +0530

    Mongodb connected

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose=require('mongoose');
+const config=require('config');
+const db=config.get('MONGOURI');
+const connectDB=async()=>{
+    try{
+        await mongoose.connect(db);
+        console.log("mongodb connected....");
+    }
+    catch(err){
+        console.log(err.message);
+        process.exit(1);
+    }
+}
+module.exports=connectDB;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express=require('express');
+const connectDB=require('./config/db');
 const app=express();
+connectDB();
 app.get('/',(req,res)=>res.send('API Running'));
 const PORT=process.env.port||5000;
 app.listen(PORT,()=>{
