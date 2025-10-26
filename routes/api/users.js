cc9b55bfa796e2faeadd4943e098ef6ee1f43471
harshitha020505 <harshithabobbala392@gmail.com>
2025-10-26 15:02:59 +0530

    updated userRoutes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,16 @@
 const express=require('express');
+const {check,validationResult}=require('express-validator');
 const router=express.Router();
-router.post('/',(req,res)=>{
+router.post('/',[
+    check('name','name is required').not().isEmpty(),
+    check('email',"enter correct email").isEmail(),
+    check('password',"enter password which have min length 6").isLength({min:6})
+],(req,res)=>{
+   const errors= validationResult(req);
+   if(!errors.isEmpty()){
+    return res.status(400).json({errors:errors.array()})
+   }
+   res.send("user route");
     console.log(req.body);
 })
 router.get('/',(req,res)=>{
